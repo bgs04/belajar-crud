c95f24ebf9f032ebd798df151a776c9dbe413154
bgs04 <bagus.riyadi555@gmail.com>
2022-03-25 18:51:25 -0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/belajar-crud/';
+$config['base_url'] = 'http://bagus-id.com/';
 
 /*
 |--------------------------------------------------------------------------
